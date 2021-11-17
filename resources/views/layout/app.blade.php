c4b22d77b97bc23fee1a1c04f5995cfda63fa7e3
saucesar <sau.cesarlima2013@gmail.com>
2021-11-17 20:57:00 -0300

    Inclusão de alerts de mensagens.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,8 +13,19 @@
 </head>
 <body>
     @include('layout.navbar')
+    <div class="container">
+        @include('components.alerts.success')
+        @include('components.alerts.error')
+    </div>
     @yield('content')
     @stack('js')
+    <script type="text/javascript">
+        window.setTimeout(function() {
+            $(".alert").fadeTo(500, 0).slideUp(500, function(){
+                $(this).remove(); 
+            });
+        }, 5000);
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
