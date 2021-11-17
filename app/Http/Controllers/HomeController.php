478d69216e0dd9d2a5d81ee913b699f13297c2a6
saucesar <sau.cesarlima2013@gmail.com>
2021-11-17 14:37:40 -0300

    Rota e metodo para lista de projetos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function projects()
+    {
+        return view('projects.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/projects', [HomeController::class, 'projects'])->name('projects');
