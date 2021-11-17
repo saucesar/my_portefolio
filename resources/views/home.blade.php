4fdda02636f6a095f61ead7ac0d89294cbe0e62e
saucesar <sau.cesarlima2013@gmail.com>
2021-11-17 14:38:10 -0300

    Ajuste do titulo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="d-flex justify-content-center">
-        <h1>Saú Cesar</h1>
+        <h1>Destaques</h1>
     </div>
 </div>
 @endsection
