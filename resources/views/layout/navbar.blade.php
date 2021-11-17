aea733c015e425c7c012db64eaf003f958a40463
saucesar <sau.cesarlima2013@gmail.com>
2021-11-17 14:37:57 -0300

    Adicao de menus com opções de projeto e contato

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -10,7 +10,13 @@
         <div class="collapse navbar-collapse" id="homeNavbar">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link text-light" aria-current="page" href="#">Projetos</a>
+                    <a class="nav-link text-light" aria-current="page" href="{{ route('home') }}">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" aria-current="page" href="{{ route('projects') }}">Projetos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" aria-current="page" href="{{ route('home') }}">Contato</a>
                 </li>
             </ul>
         </div>
