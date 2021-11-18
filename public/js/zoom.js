4823afb21fc312c32fe817ddf095dec82a2a6cbb
saucesar <sau.cesarlima2013@gmail.com>
2021-11-18 00:09:05 -0300

    Arquivo js com funções de zoom para reuso.

---
+++ public/js/zoom.js
@@ -0,0 +1,9 @@
+function zoom_in(card) {
+    card.classList.add("shadow-lg")
+    card.classList.add("card-zoom")
+}
+
+function zoom_out(card) {
+    card.classList.remove("shadow-lg")
+    card.classList.remove("card-zoom")
+}
