99a3cfbe0c8aa21a45896458b554282819cdc2c9
saucesar <sau.cesarlima2013@gmail.com>
2021-11-18 00:09:54 -0300

    Uso de script em arquivo separado

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -16,15 +16,5 @@
 @endsection
 
 @push('js')
-<script type="text/javascript">
-    function zoom_in(card) {
-        card.classList.add("shadow-lg")
-        card.classList.add("card-zoom")
-    }
-
-    function zoom_out(card) {
-        card.classList.remove("shadow-lg")
-        card.classList.remove("card-zoom")
-    }
-</script>
+<script type="text/javascript" src="{{ asset('js/zoom.js') }}"></script>
 @endpush
