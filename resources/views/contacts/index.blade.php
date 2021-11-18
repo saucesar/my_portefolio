644f12c66fc122963120906a3f5301b171009a51
saucesar <sau.cesarlima2013@gmail.com>
2021-11-17 21:27:39 -0300

    tela para informações de contato

---
+++ resources/views/contacts/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app', ['active' => 'contacts'])
+
+@section('content')
+<div class="container">
+    <div class="d-flex justify-content-center">
+        <h1>Fale comigo</h1>
+    </div>
+</div>
+@endsection
