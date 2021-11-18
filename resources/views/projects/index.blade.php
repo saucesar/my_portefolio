f0fd33a297f1882b26c93d3e48cfaeee2698f7d6
saucesar <sau.cesarlima2013@gmail.com>
2021-11-17 23:36:54 -0300

    Inclusao de cards de projeto e js para animação de zoom no card.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,8 +2,29 @@
 
 @section('content')
 <div class="container">
-    <div class="d-flex justify-content-center">
+    <div class="d-flex justify-content-center mb-5">
         <h1>Projetos</h1>
+    </div>
+    <div class="row row-cols-lg-4 row-cols-md-3 row-cols-sm-1">
+        @foreach ($projects as $project)
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            @include('projects.card')
+        </div>
+        @endforeach
     </div>
 </div>
 @endsection
+
+@push('js')
+<script type="text/javascript">
+    function zoom_in(card) {
+        card.classList.add("shadow-lg")
+        card.classList.add("card-zoom")
+    }
+
+    function zoom_out(card) {
+        card.classList.remove("shadow-lg")
+        card.classList.remove("card-zoom")
+    }
+</script>
+@endpush
