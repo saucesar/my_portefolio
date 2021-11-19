8993e7291e6f35bad41c8c3ee1cf5fba98a37b3e
saucesar <sau.cesarlima2013@gmail.com>
2021-11-19 14:05:22 -0300

    Modal para crud de projeto

---
+++ resources/views/projects/modal.blade.php
@@ -0,0 +1,20 @@
+<!-- Modal -->
+<div class="modal fade" id="{{ $id }}" tabindex="-1" aria-labelledby="{{ $id }}Label" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="{{ $id }}Label">{{ $modal_title }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="@if(isset($project)) {{ route('projects.update', $project->id) }} @else {{ route('projects.store') }} @endif">
+                <div class="modal-body">
+                @include('projects.form')
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
