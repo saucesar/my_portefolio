a109387668adf60ad762d7d8eb2118e88a62bb67
saucesar <sau.cesarlima2013@gmail.com>
2021-11-19 14:05:43 -0300

    Inclusão de menu de opções.

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -19,6 +19,24 @@
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 card-project-description d-flex flex-column">
+                @if(auth()->user())
+                <div class="card card-body mb-2">
+                    <h5 class="text-center">Opções</h5>
+                    <div class="d-inline-flex justify-content-center">
+                        <button type="button" class="btn btn-sm btn-warning text-light me-2" data-bs-toggle="modal" data-bs-target="#project-edit-{{ $project->id }}" title="Editar">
+                            <i class="fas fa-pen-square"></i>
+                        </button>
+                        <form action="{{ route('projects.destroy', $project->id) }}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button class="btn btn-sm btn-danger" type="submit" onclick="return confirm('Tem certeza');" title="Deletar">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </form>
+                        @include('projects.modal', ['id' => 'project-edit-'.($project->id), 'modal_title' => 'Atualizar projeto'])
+                    </div>
+                </div>
+                @endif
                 <div class="card card-body mb-2">
                     <h5 class="text-center">Tecnologias</h5>
                     <div class="d-flex justify-content-center">
