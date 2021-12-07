8197772e108e86d069a3414f3a18b969ebee0a4c
saucesar <sau.cesarlima2013@gmail.com>
2021-12-07 18:07:07 -0300

    Ajuste para centralização dos elementos do card

--- resources/views/projects/tecnologies.blade.php
+++ resources/views/projects/tecnologies.blade.php
@@ -2,10 +2,12 @@
     <h5 class="text-center">Tecnologias</h5>
     <div class="d-flex justify-content-center">
         @foreach ($tecnologies as $tecnology)
-        <div class="card card-body d-flex justify-content-center" >
-            <a href="@if(isset($tecnology->link)) {{ $tecnology->link }} @else # @endif" @if(isset($tecnology->link)) target="_blank" @endif >
-                <img src="{{ $tecnology->icon->url() }}" width="30px" title="{{ $tecnology->name }}" alt="tecnology-icon">
-            </a>
+        <div class="card card-body">
+            <div class="d-flex justify-content-center">
+                <a href="@if(isset($tecnology->link)) {{ $tecnology->link }} @else # @endif" @if(isset($tecnology->link)) target="_blank" @endif >
+                    <img src="{{ $tecnology->icon->url() }}" width="30px" title="{{ $tecnology->name }}" alt="tecnology-icon">
+                </a>
+            </div>
         </div>
         @endforeach
     </div>
