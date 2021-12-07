cfcc817b22240f863be0d8a54b5d23f85e196e58
saucesar <sau.cesarlima2013@gmail.com>
2021-12-07 16:45:31 -0300

    Ajuste de diretivar de csrf do laravel e para atualização de projeto

--- resources/views/projects/modal.blade.php
+++ resources/views/projects/modal.blade.php
@@ -6,8 +6,12 @@
                 <h5 class="modal-title" id="{{ $id }}Label">{{ $modal_title }}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form action="@if(isset($project)) {{ route('projects.update', $project->id) }} @else {{ route('projects.store') }} @endif">
+            <form action="@if(isset($project)) {{ route('projects.update', $project->id) }} @else {{ route('projects.store') }} @endif" method="POST">
                 <div class="modal-body">
+                @if(isset($project))
+                @method('put') 
+                @endif
+                @csrf
                 @include('projects.form')
                 </div>
                 <div class="modal-footer">
