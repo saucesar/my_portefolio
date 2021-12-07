62d20126d684d5928f8d2d00914f207cb9db5cad
saucesar <sau.cesarlima2013@gmail.com>
2021-12-07 20:46:33 -0300

    Ajuste na posição do stack js

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -22,9 +22,12 @@
         @include('components.alerts.error')
     </div>
     @yield('content')
-    @stack('js')
+    
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    
+    @stack('js')
+    
     <script type="text/javascript">
         window.setTimeout(function() {
             $(".alert").fadeTo(500, 0).slideUp(500, function(){
