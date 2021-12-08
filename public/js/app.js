d7e9b10064bd987d0dc5403b8c7ac367035d307b
saucesar <sau.cesarlima2013@gmail.com>
2021-12-07 23:02:34 -0300

    Função para confirmar a deleção de imagem.

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,15 @@
 function goto(link){
     window.location.href = link;
 }
+
+function confirmImageDelete(formId) {
+    const form = document.getElementById(formId);
+
+    if(form) {
+        let destroy = confirm('Tem certeza?');
+
+        if(destroy) {
+            form.submit();
+        }
+    }
+}
